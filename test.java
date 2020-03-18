51fcc24aedc6184e258636547d3d438d64b47b23
yelinfengyuebuqun <62238211+yelinfengyuebuqun@users.noreply.github.com>
2020-03-18 15:12:48 +0800

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ package com.example.test;
 
 public class test {
     public static void main(String args[]){
-        System.out.println("你好啊，岳不群！");
+        System.out.println("你好啊，岳不群！我是你大爷");
     }
 }
